69ffcc8ed6877182f63952ee8d4b5b72e8824b62
Germán Jurado <juniorjurado2004@gmail.com>
2025-06-09 22:01:26 -0500

    ├Ültimo Commit

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -20,7 +20,7 @@ func Manejadores(path string, method string, body string, headers map[string] st
 		return statusCode, user
 	}
 
-	fmt.Println(" > Path: "+ path[0:4])
+	fmt.Println(" > Path: "+ path[1:5])
 
 	switch path[0:4] {
 	case "user":
