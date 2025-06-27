74dca53746fb648bb9868b264a0bd4bca371ae96
Germán Jurado <juniorjurado2004@gmail.com>
2025-06-26 22:08:17 -0500

    Ultimo Commit

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,11 +3,9 @@ package handlers
 import (
 	"fmt"
 	"strconv"
-
 	"github.com/Junior_Jurado/gambit/auth"
 	"github.com/Junior_Jurado/gambit/routers"
 	"github.com/aws/aws-lambda-go/events"
-	"golang.org/x/text/cases"
 )
 
 func Manejadores(path string, method string, body string, headers map[string] string, request events.APIGatewayV2HTTPRequest) (int, string) {
